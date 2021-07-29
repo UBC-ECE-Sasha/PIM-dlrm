175e874ab3081b4765553b4db042a6e745901235
Niloofar Zarif <nzarif@users.noreply.github.com>
2021-07-28 18:30:59 -0700

    Update run.sh
    Added configs from DeepRecSys for 3 classes of DLRM models

--- run.sh
+++ run.sh
@@ -13,12 +13,44 @@ python dlrm_s_pytorch.py     \
 --mini-batch-size=32     \
 --num-batches=10     \
 --data-generation=random     \
---arch-mlp-bot=800-1024-1024-1024-64    \
---arch-mlp-top=1024-1024-1024-1024-1     \
---arch-sparse-feature-size=64    \
---arch-embedding-size=$TABLES \
---num-indices-per-lookup=10    \
---num-indices-per-lookup-fixed=True   \
+
+#RMC1
+: ' 
+--arch_mlp_bot=128-64-32    \
+--arch_mlp_top=256-64-1     \
+--arch_embedding_size=4000000-4000000-4000000-4000000-4000000-4000000-4000000-4000000   \
+--arch_sparse_feature_size=32    \
+--num_indices_per_lookup_fixed=true     \
+--num_indices_per_lookup=80     \
+--arch_interaction_op=cat   \
+--model_type=dlrm   \
+--model_name=rm1    \
+'
+
+#RMC2
+--arch_mlp_bot=256-128-64    \
+--arch_mlp_top=128-64-1     \ 
+--arch_embedding_size=500000-500000-500000-500000-500000-500000-500000-500000-500000-500000-500000-500000-500000-500000-500000-500000-500000-500000-500000-500000-500000-500000-500000-500000-500000-500000-500000-500000-500000-500000-500000-500000   \
+--arch_sparse_feature_size=64    \
+--num_indices_per_lookup_fixed=true     \
+--num_indices_per_lookup=120   \
+--arch_interaction_op=cat   \
+--model_type=dlrm   \
+--model_name=rm2    \
+
+#RMC3
+: '
+--arch_mlp_bot=2560-1024-256-32     \
+--arch_mlp_top=512-256-1    \
+--arch_embedding_size=2000000-2000000-2000000-2000000-2000000-2000000-2000000-2000000-2000000-2000000    \
+--arch_sparse_feature_size=32   \
+--num_indices_per_lookup_fixed=true     \
+--num_indices_per_lookup=20     \
+--arch_interaction_op=cat   \
+--model_type=dlrm   \
+--model_name=rm3    \
+'
+
 --print-freq=1    \
 --print-time  \
 --deeprecsys_load_gen \
